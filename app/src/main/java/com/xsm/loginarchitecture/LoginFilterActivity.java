43fca9a6ac41dd05008d799f1ac54e148e7b3f37
xiashengming <xiasem@163.com>
2018-09-28 17:26:45 +0800

    架构修改

--- app/src/main/java/com/xsm/loginarchitecture/LoginFilterActivity.java
+++ app/src/main/java/com/xsm/loginarchitecture/LoginFilterActivity.java
@@ -5,19 +5,18 @@ import android.os.Bundle;
 import android.widget.Toast;
 
 import com.xsm.loginarchitecture.lib_login.annotation.LoginFilter;
+import com.xsm.loginarchitecture.lib_login.core.LoginAssistant;
 
 public class LoginFilterActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (lib_login_filter_onCreate(true)) {
+        if (!LoginAssistant.getInstance().getiLogin().isLogin(getApplicationContext())) {
             //TODO: 你可以想做什么就做什么，在这里我让页面结束，并给用户提示
             Toast.makeText(this, "没有登录！", Toast.LENGTH_SHORT).show();
             finish();
         }
     }
 
-    @LoginFilter
-    public Boolean lib_login_filter_onCreate(Boolean aspectParam) { return aspectParam; }
 }
